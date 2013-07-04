c780541a502193c751ddf4838ef082d16af8bec4
Steven Willems <steven.willems@i-develop.be>
2013-07-04 12:38:41 +0200

    ObjectSerializer should be final

--- src/main/java/be/idevelop/fiber/ObjectSerializer.java
+++ src/main/java/be/idevelop/fiber/ObjectSerializer.java
@@ -33,7 +33,7 @@ import java.util.TreeSet;
 
 import static be.idevelop.fiber.ObjectCreator.OBJECT_CREATOR;
 
-public class ObjectSerializer<T> extends Serializer<T> {
+public final class ObjectSerializer<T> extends Serializer<T> {
 
     private final SortedSet<Field> fields;
 
