531dc6197afd3c3b9e3cd55ef64e9b03a58023f3
Steven Willems <steven.willems@i-develop.be>
2013-07-04 16:27:02 +0200

    Optimization of ByteBuffer allocation * added performance test * added ByteBuffer ring of standard 128 Mb

--- src/main/java/be/idevelop/fiber/ByteBufferRing.java
+++ src/main/java/be/idevelop/fiber/ByteBufferRing.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Steven Willems
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package be.idevelop.fiber;
 
 import java.nio.ByteBuffer;
@@ -8,9 +32,18 @@ enum ByteBufferRing {
 
     private static final int MB_128 = 1024 * 1024 * 128;
 
-    private final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(MB_128);
+    int start;
 
-    int start = byteBuffer.position();
+    private ByteBuffer byteBuffer;
+
+    private ByteBufferRing() {
+        size(MB_128);
+    }
+
+    public synchronized void size(int newSize) {
+        byteBuffer = ByteBuffer.allocateDirect(newSize);
+        start = byteBuffer.position();
+    }
 
     public synchronized ByteBuffer allocate(int bufferSize) {
         if (byteBuffer.limit() - start < bufferSize) {
